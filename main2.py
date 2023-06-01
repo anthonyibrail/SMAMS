5c97c42d9b9347b811747ddb528b7e5649c18d2f
moyhatake <milesmoises@hotmail.com>
2023-05-31 22:34:38 -0600

    Interface updated

--- main2.py
+++ main2.py
@@ -12,8 +12,11 @@ from string import ascii_uppercase
 import tkinter as tk
 from PIL import Image, ImageTk
 
-from hunspell import Hunspell
-import enchant
+#from hunspell import Hunspell
+#import enchant
+
+from gtts import gTTS
+from playsound import playsound
 
 from keras.models import model_from_json
 
@@ -21,44 +24,55 @@ os.environ["THEANO_FLAGS"] = "device=cuda, assert_no_cpu_op=True"
 
 class Application:
     def __init__(self):
-        self.hs = Hunspell('en_US')
+        #self.hs = Hunspell('en_US')
         self.vs = cv2.VideoCapture(0)
         self.current_image = None
         self.current_image2 = None
-        self.json_file = open("Models\model_new.json", "r")
+        self.json_file = open("Models\model_new_5.json", "r")
         self.model_json = self.json_file.read()
         self.json_file.close()
 
         self.loaded_model = model_from_json(self.model_json)
-        self.loaded_model.load_weights("Models\model_new.h5")
-
-        self.json_file_dru = open("Models\model-bw_dru.json" , "r")
-        self.model_json_dru = self.json_file_dru.read()
-        self.json_file_dru.close()
-
-        self.loaded_model_dru = model_from_json(self.model_json_dru)
-        self.loaded_model_dru.load_weights("Models\model-bw_dru.h5")
-        self.json_file_tkdi = open("Models\model-bw_tkdi.json" , "r")
-        self.model_json_tkdi = self.json_file_tkdi.read()
-        self.json_file_tkdi.close()
-
-        self.loaded_model_tkdi = model_from_json(self.model_json_tkdi)
-        self.loaded_model_tkdi.load_weights("Models\model-bw_tkdi.h5")
-        self.json_file_smn = open("Models\model-bw_smn.json" , "r")
-        self.model_json_smn = self.json_file_smn.read()
-        self.json_file_smn.close()
-
-        self.loaded_model_smn = model_from_json(self.model_json_smn)
-        self.loaded_model_smn.load_weights("Models\model-bw_smn.h5")
+        self.loaded_model.load_weights("Models\model_new_5.h5")
 
         self.ct = {}
         self.ct['blank'] = 0
         self.blank_flag = 0
 
+        self.expressions = {
+            "Estrellas brillantes iluminan el espacio.", 
+            "Luna, nuestro vecino nocturno.", 
+            "Agujeros negros: misterio infinito.",
+            "Gravedad distinta, reglas cambian.",
+            "Vía Láctea, nuestra galaxia hogar.",
+            "Aire ausente, trajes indispensables.",
+            "Radiación cósmica, peligro omnipresente.",
+            "Telescopios desvelan secretos celestiales.",
+            "Silencio absoluto, vacío infinito.",
+            "",
+            "",
+            "Espacio hostil, vida imposible.",
+            "Nebulosas: gas y polvo estelar.",
+            "Estrellas ardientes, gigantes incandescentes.",
+            "Galaxias, sistemas solares interminables.",
+            "Sol, estrella radiante y poderosa.",
+            "Asombro y maravilla, espacio infinito.",
+            "Cometas: viajeros celestiales errantes.",
+            "Espacio profundo, territorio inexplorado.",
+            "Gravedad cero, flotar en espacio.",
+            "Exploración espacial, comprensión universal.",
+            "Estrellas fugaces, deseos cumplidos.",
+            "Universo vasto, infinitas posibilidades.",
+            "Estación Espacial Internacional: laboratorio orbital.",
+            "Exploración espacial, progreso científico importante.",
+            "Misterio cósmico infinito.",
+            ""
+        }
+
         for i in ascii_uppercase:
           self.ct[i] = 0
         
-        print("Loaded model from disk")
+        print("Model loaded")
 
         self.root = tk.Tk()
         self.root.title("SMAMS")
@@ -82,24 +96,28 @@ class Application:
         self.panel_mode.place(x = 705 - 40, y = 120, width = 150, height = 100)
         self.panel_mode.configure(background = "#1F2937")
 
-        mode_option = tk.IntVar()
-        self.rb_mode1 = tk.Radiobutton(self.panel_mode, text = "Palabras", variable = mode_option, value = 1, command = lambda: print(mode_option.get()), indicatoron = False, selectcolor = "#4A569D")
-        self.rb_mode2 = tk.Radiobutton(self.panel_mode, text = "Frases", variable = mode_option, value = 2, command = lambda: print(mode_option.get()), indicatoron = False, selectcolor = "#ED4264")
-        mode_option.set(1)
+        self.mode_option = tk.IntVar()
+        self.rb_mode1 = tk.Radiobutton(self.panel_mode, text = "Palabras", variable = self.mode_option, value = 1, command = self.mode_1, indicatoron = False, selectcolor = "#4A569D")
+        self.rb_mode2 = tk.Radiobutton(self.panel_mode, text = "Frases", variable = self.mode_option, value = 2, command = self.mode_2, indicatoron = False, selectcolor = "#ED4264")
+        self.mode_option.set(1)
         self.rb_mode1.configure(width = 50, compound = 'left', background = "#1F2937", font = ("YouTube-Sans-Bold", 20), foreground = "#DFE4EA")
         self.rb_mode2.configure(width = 50, compound = 'left', background = "#1F2937", font = ("YouTube-Sans-Bold", 20), foreground = "#DFE4EA")
         self.rb_mode1.pack()
         self.rb_mode2.pack()
 
-        self.btn_speak = tk.Button(self.root, text = "Borrar último", command = self.speak, relief = tk.GROOVE)
-        self.btn_speak.place(x = 700 - 40, y = 360, width = 165)
-        self.btn_speak.configure(background = "#14181D", font = ("YouTube-Sans-Light", 20), foreground = "#DFE4EA", borderwidth = 0)
+        self.btn_add = tk.Button(self.root, text = "Agregar", command = self.add_char, relief = tk.GROOVE)
+        self.btn_add.place(x = 700 - 40, y = 300, width = 165)
+        self.btn_add.configure(background = "#ED4264", font = ("YouTube-Sans-Bold", 20), foreground = "#DFE4EA", borderwidth = 0)        
 
-        self.btn_speak = tk.Button(self.root, text = "Borrar todo", command = self.speak, relief = tk.GROOVE)
-        self.btn_speak.place(x = 700 - 40, y = 420, width = 165)
-        self.btn_speak.configure(background = "#14181D", font = ("YouTube-Sans-Light", 20), foreground = "#DFE4EA", borderwidth = 0)
+        self.btn_dlt_1 = tk.Button(self.root, text = "Borrar último", command = self.cls_last, relief = tk.GROOVE)
+        self.btn_dlt_1.place(x = 700 - 40, y = 360, width = 165)
+        self.btn_dlt_1.configure(background = "#14181D", font = ("YouTube-Sans-Light", 20), foreground = "#DFE4EA", borderwidth = 0)
 
-        self.btn_speak = tk.Button(self.root, text = "Play", command = self.speak, relief = tk.GROOVE)
+        self.btn_dlt_2 = tk.Button(self.root, text = "Borrar todo", command = self.cls_all, relief = tk.GROOVE)
+        self.btn_dlt_2.place(x = 700 - 40, y = 420, width = 165)
+        self.btn_dlt_2.configure(background = "#14181D", font = ("YouTube-Sans-Light", 20), foreground = "#DFE4EA", borderwidth = 0)
+
+        self.btn_speak = tk.Button(self.root, text = "Play", command = self.player, relief = tk.GROOVE)
         self.btn_speak.place(x = 700 - 40, y = 480, width = 165)
         self.btn_speak.configure(background = "#14181D", font = ("YouTube-Sans-Bold", 20), foreground = "#DFE4EA", borderwidth = 0)
 
@@ -108,40 +126,18 @@ class Application:
         self.T.config(text = "Show Me A Mexican Sign", font = ("YouTube-Sans-Bold", 30, "bold"), fg = "#DFE4EA", background = "#1F2937")
 
         self.panel3 = tk.Label(self.root) # Current Character
-        self.panel3.place(x = 215, y = 540)
+        self.panel3.place(x = 215, y = 550)
 
         self.T1 = tk.Label(self.root)
         self.T1.place(x = 60, y = 540)
         self.T1.config(text = "Letra:", font = ("YouTube-Sans-Bold", 30, "bold"), fg = "#DFE4EA", background = "#1F2937")
 
-        self.panel4 = tk.Label(self.root) # Word/Expression
-        self.panel4.place(x = 210, y = 595)
+        self.panel4 = tk.Label(self.root) # Word
+        self.panel4.place(x = 210, y = 605)
 
         self.T2 = tk.Label(self.root)
         self.T2.place(x = 60, y = 595)
         self.T2.config(text = "Palabra:", font = ("YouTube-Sans-Bold", 30, "bold"), fg = "#DFE4EA", background = "#1F2937")
-
-        """
-        self.panel5 = tk.Label(self.root) # Sentence
-        self.panel5.place(x = 350, y = 645)
-
-        self.T3 = tk.Label(self.root)
-        self.T3.place(x = 60, y = 645)
-        self.T3.config(text = "Oración:", font = ("YouTube-Sans-Bold", 30, "bold"), fg = "#DFE4EA", background = "#1F2937")
-
-        self.T4 = tk.Label(self.root)
-        self.T4.place(x = 250, y = 690)
-        self.T4.config(text = "Sugerencias:", font = ("YouTube-Sans-Bold", 30, "bold"), fg = "#4A569D", background = "#1F2937")
-
-        self.bt1 = tk.Button(self.root, command = self.action1, height = 0, width = 0)
-        self.bt1.place(x = 26, y = 745)
-
-        self.bt2 = tk.Button(self.root, command = self.action2, height = 0, width = 0)
-        self.bt2.place(x = 325, y = 745)
-
-        self.bt3 = tk.Button(self.root, command = self.action3, height = 0, width = 0)
-        self.bt3.place(x = 625, y = 745)
-        """
 
         self.str = ""
         self.word = " "
@@ -188,41 +184,19 @@ class Application:
             self.panel2.imgtk = imgtk
             self.panel2.config(image = imgtk)
 
-            self.panel3.config(text = self.current_symbol, font = ("YouTube-Sans-Bold", 30), fg = "#ED4264", background = "#1F2937")
-            self.panel4.config(text = self.word, font = ("YouTube-Sans-Light", 30), fg = "#F0F3F7", background = "#1F2937")
-            """
-            self.panel5.config(text = self.str,font = ("YouTube-Sans-Light", 30), fg = "#F0F3F7", background = "#1F2937")
-
-            predicts = self.hs.suggest(self.word)
-            
-            if(len(predicts) > 1):
-                self.bt1.config(text = predicts[0], font = ("YouTube Sans Medium", 20))
+            self.panel3.config(text = self.current_symbol, font = ("YouTube-Sans-Bold", 20), fg = "#ED4264", background = "#1F2937")
+            if self.mode_option.get() == 1:
+                self.panel4.config(text = self.word, font = ("YouTube-Sans-Light", 20), fg = "#F0F3F7", background = "#1F2937")
             else:
-                self.bt1.config(text = "")
-
-            if(len(predicts) > 2):
-                self.bt2.config(text = predicts[1], font = ("YouTube Sans Medium", 20))
-            else:
-                self.bt2.config(text = "")
-
-            if(len(predicts) > 3):
-                self.bt3.config(text = predicts[2], font = ("YouTube Sans Medium", 20))
-            else:
-                self.bt3.config(text = "")
-            """
+                self.panel4.config(text = self.word, font = ("YouTube-Sans-Light", 20), fg = "#1F2937", background = "#1F2937")
+                
 
         self.root.after(5, self.video_loop)
-
+    
     def predict(self, test_image):
         test_image = cv2.resize(test_image, (128, 128))
 
         result = self.loaded_model.predict(test_image.reshape(1, 128, 128, 1))
-
-        result_dru = self.loaded_model_dru.predict(test_image.reshape(1 , 128 , 128 , 1))
-
-        result_tkdi = self.loaded_model_tkdi.predict(test_image.reshape(1 , 128 , 128 , 1))
-
-        result_smn = self.loaded_model_smn.predict(test_image.reshape(1 , 128 , 128 , 1))
 
         prediction = {}
 
@@ -230,157 +204,84 @@ class Application:
 
         inde = 1
 
-        for i in ascii_uppercase:
-            prediction[i] = result[0][inde]
-            inde += 1
-
-        #LAYER 1
+        if self.mode_option.get() == 1:
+            for i in ascii_uppercase:
+                prediction[i] = result[0][inde]
+                inde += 1
+        else:
+            for i in self.expressions:
+                prediction[i] = result[0][inde]
+                inde += 1
 
         prediction = sorted(prediction.items(), key = operator.itemgetter(1), reverse = True)
-
         self.current_symbol = prediction[0][0]
 
-        #LAYER 2
+    # Callbacks
+    def mode_1(self):
+        self.mode_option.set(1)
+        self.btn_add.configure(state = 'normal')
+        self.btn_dlt_1.configure(state = 'normal')
+        self.btn_dlt_2.configure(state = 'normal')
+        self.T1.configure(text = "Letra:")
+        self.T2.configure(fg = "#DFE4EA")
+        self.panel4.configure(fg = "#F0F3F7")
 
-        if(self.current_symbol == 'D' or self.current_symbol == 'R' or self.current_symbol == 'U'):
+    def mode_2(self):
+        self.mode_option.set(2)
+        self.btn_add.configure(state = 'disabled')
+        self.btn_dlt_1.configure(state = 'disabled')
+        self.btn_dlt_2.configure(state = 'disabled')
+        self.T1.configure(text = "Frase:")
+        self.T2.configure(fg = "#1F2937")
+        self.panel4.configure(fg = "#1F2937")
 
-        	prediction = {}
+    def add_char(self):
+        self.word += self.current_symbol
 
-        	prediction['D'] = result_dru[0][0]
-        	prediction['R'] = result_dru[0][1]
-        	prediction['U'] = result_dru[0][2]
+    def cls_last(self):
+        try:
+            self.word = self.word[0: len(self.word) - 1]
+        except: print("")
 
-        	prediction = sorted(prediction.items(), key = operator.itemgetter(1), reverse = True)
+    def cls_all(self):
+        self.word = ""
 
-        	self.current_symbol = prediction[0][0]
+    def player(self):
+        if self.mode_option.get() == 1:
+            #if self.word != "":
+            try:
+                tts = gTTS(text = self.word, lang = 'es')
+                tts.save("output.mp3")
+                full_path = os.path.abspath("output.mp3")
+                time.sleep(1)
+                playsound(full_path)
+            except: print("")
+        else:
+            #if self.current_symbol != "":
+            try:
+                tts = gTTS(text = self.current_symbol , lang = 'es')
+                tts.save("output.mp3")
+                full_path = os.path.abspath("output.mp3")
+                time.sleep(1)
+                playsound(full_path)
+            except: print("")
 
-        if(self.current_symbol == 'D' or self.current_symbol == 'I' or self.current_symbol == 'K' or self.current_symbol == 'T'):
-
-        	prediction = {}
-
-        	prediction['D'] = result_tkdi[0][0]
-        	prediction['I'] = result_tkdi[0][1]
-        	prediction['K'] = result_tkdi[0][2]
-        	prediction['T'] = result_tkdi[0][3]
-
-        	prediction = sorted(prediction.items(), key = operator.itemgetter(1), reverse = True)
-
-        	self.current_symbol = prediction[0][0]
-
-        if(self.current_symbol == 'M' or self.current_symbol == 'N' or self.current_symbol == 'S'):
-
-        	prediction1 = {}
-
-        	prediction1['M'] = result_smn[0][0]
-        	prediction1['N'] = result_smn[0][1]
-        	prediction1['S'] = result_smn[0][2]
-
-        	prediction1 = sorted(prediction1.items(), key = operator.itemgetter(1), reverse = True)
-
-        	if(prediction1[0][0] == 'S'):
-        		self.current_symbol = prediction1[0][0]
-        	else:
-        		self.current_symbol = prediction[0][0]
-        
-        if(self.current_symbol == 'blank'):
-            for i in ascii_uppercase:
-                self.ct[i] = 0
-
-        self.ct[self.current_symbol] += 1
-
-        if(self.ct[self.current_symbol] > 60):
-            for i in ascii_uppercase:
-                if i == self.current_symbol:
-                    continue
-
-                tmp = self.ct[self.current_symbol] - self.ct[i]
-
-                if tmp < 0:
-                    tmp *= -1
-
-                if tmp <= 20:
-                    self.ct['blank'] = 0
-
-                    for i in ascii_uppercase:
-                        self.ct[i] = 0
-                    return
-
-            self.ct['blank'] = 0
-
-            for i in ascii_uppercase:
-                self.ct[i] = 0
-
-            if self.current_symbol == 'blank':
-                if self.blank_flag == 0:
-                    self.blank_flag = 1
-
-                    if len(self.str) > 0:
-                        self.str += " "
-
-                    self.str += self.word
-
-                    self.word = ""
-            else:
-                if(len(self.str) > 16):
-                    self.str = ""
-
-                self.blank_flag = 0
-
-                self.word += self.current_symbol
-
-    def speak(self):
-         print("")
-    """
-    def action1(self):
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 0):
-            self.word = ""
-
-            self.str += " "
-
-            self.str += predicts[0]
-
-    def action2(self):
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 1):
-            self.word = ""
-            self.str += " "
-            self.str += predicts[1]
-
-    def action3(self):
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 2):
-            self.word = ""
-            self.str += " "
-            self.str += predicts[2]
-
-    def action4(self):
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 3):
-            self.word = ""
-            self.str += " "
-            self.str += predicts[3]
-
-    def action5(self):
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 4):
-            self.word = ""
-            self.str += " "
-            self.str += predicts[4]
-    """
+    def space(self):
+        interrupt = cv2.waitKey(10)
+        if interrupt & 0xFF == 32:
+            self.word += " "
             
     def destructor(self):
-        print("Closing Application...")
+        print("Closing App...")
+        try:
+            full_path = os.path.abspath("output.mp3")
+            os.remove(full_path)
+        except: print("")
 
         self.root.destroy()
         self.vs.release()
         cv2.destroyAllWindows()
     
-print("Starting Application...")
+print("Starting App...")
 
 (Application()).root.mainloop()
